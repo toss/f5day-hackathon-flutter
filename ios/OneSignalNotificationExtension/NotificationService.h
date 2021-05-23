e8a2c23c5ed0a31efa88b614b382cf41d6c68586
younghyup <dbsdudguq@gmail.com>
2021-05-23 17:36:02 +0700

    ios build set. push

---
+++ ios/OneSignalNotificationExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationExtension
+//
+//  Created by 윤영협 on 2021/05/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
